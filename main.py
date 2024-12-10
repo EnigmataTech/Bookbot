b45acbf2e4ffdb18a22cd430aaf645210f67e7ed
EnigmataTech <EnigmataTech@protonmail.com>
2024-12-10 18:41:49 -0400

    added a function to count and track unique characters present in the extracted text

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 def main():
-    book_path = "github.com/EnigmataTech/Bookbot/books/frankenstein.txt"
+    book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    # print(text)
-    print(f"{num_words} words found in the document")
+    chars_dict = get_chars_dict(text)
+    #print(text)
+    print(chars_dict)
+    
+
+def get_chars_dict(text):
+    chars = {}
+    for char in text:
+        lower = char.lower()
+        if lower in chars:
+            chars[lower] += 1
+        else:
+            chars[lower] = 1
+    return chars   
     
 def get_num_words(text):
     words = text.split()
